f1c0641ef68428ff87d506cafd5b189a53dc2201
ee07b505 <gym_009@163.com>
2023-07-18 09:13:50 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const ethers = require('ethers');
 const zksync = require('zksync-web3');
 const ZKSYNC_ERA_URL = "https://rpc.ankr.com/zksync_era"
 const zk_provier = new zksync.Provider(ZKSYNC_ERA_URL)
-const eth_provier = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/e014a014a2c0479e957e62810307114b")
 const privateFile = 'private.txt'
 const fs = require('fs');
+const { error } = require('console');
 const pdata = fs.readFileSync(privateFile, 'utf8');
 const pdata_array = pdata.split("\n");
 const wallet_privatekeys = pdata_array.filter(Boolean)
@@ -31,7 +31,6 @@ function sleep(seconds) {
 ];
     const contractAddress = '0x1BbD33384869b30A323e15868Ce46013C82B86FB';
 
-    try {
 
         console.time('EraLend');
         // 创建钱包
@@ -54,9 +53,8 @@ function sleep(seconds) {
             return tx.transactionHash;
             }
         console.log("EraLend ETH 没有足够余额可供充值")
-    } catch (error) {
-        console.log(error);
-    }
+        throw new error("raLend ETH 没有足够余额可供充值")
+
 }
 
 
@@ -80,15 +78,12 @@ async function eralend_enterMarkets(wallet) {
     }]
     const contractAddress = '0x0171cA5b372eb510245F5FA214F5582911934b3D';
     const cTokens =['0x1BbD33384869b30A323e15868Ce46013C82B86FB']
-    try {
     const contract = new zksync.Contract(contractAddress, abi, wallet);
     let response = await contract.enterMarkets(cTokens);
     let tx = await response.wait();
     console.log('EraLend 进入市场充当抵押物成功', tx.transactionHash);
     return tx.transactionHash;
-    }catch (error) {
-        console.log(error);
-    }
+
 }
 
 
@@ -111,7 +106,6 @@ async function eralend_borrow(borrowAmount,wallet) {
         "type": "function"
     }]
     const contractAddress = '0x1BbD33384869b30A323e15868Ce46013C82B86FB';
-    try {
     const contract = new zksync.Contract(contractAddress, abi, wallet);
     let gasLimit = await contract.estimateGas.borrow(borrowAmount);
     gasLimit = gasLimit.mul(6).div(10); // 60% gas limit
@@ -119,14 +113,14 @@ async function eralend_borrow(borrowAmount,wallet) {
     let tx = await response.wait();
     console.log('EraLend 借款成功', tx.transactionHash);
     return tx.transactionHash;
-    }
-    catch (error) {
-        console.log(error);
-    }
+
 }
 async function main(){
+
     wallet_privatekeys.forEach(async function(privatekey){
-        const wallet = new zksync.Wallet(privatekey, zk_provier,eth_provier);
+        try {
+
+        const wallet = new zksync.Wallet(privatekey, zk_provier);
         const deposit_amount = ethers.utils.parseEther("0.03");
         const borrow_amount = ethers.utils.parseEther("0.017");
         // which address begin to deposit and borrow 
@@ -143,8 +137,15 @@ async function main(){
         console.log(`执行成功 https://explorer.zksync.io/tx/${borrow_tx}} \n`);
         await sleep(2);
 
+
     }
-    )
+
+    catch (error) {
+        console.log(`wallet address: ${wallet.address} 执行失败`);
+        console.log(error);
+    }
+ } )
+
 }
 main()
 
